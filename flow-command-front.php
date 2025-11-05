3d18c7f3c73d259f104942a6f8da37bab4ac6c38
fenicmoi <phatthalung.it@gmail.com>
2025-11-05 07:52:16 +0700

    แก้ไขภาษาไทยให้ datatable

--- flow-command-front.php
+++ flow-command-front.php
@@ -7,6 +7,20 @@
           order: [[ 0, 'desc' ], [ 0, 'asc' ]],
 					"processing": true,
 					"serverSide": true,
+          "language": {
+                "sLengthMenu": "แสดง _MENU_ เร็คคอร์ด ต่อหน้า",
+                "sZeroRecords": "ไม่พบข้อมูลที่ค้นหา",
+                "sInfo": "แสดง _START_ ถึง _END_ ของ _TOTAL_ เร็คคอร์ด",
+                "sInfoEmpty": "แสดง 0 ถึง 0 ของ 0 เร็คคอร์ด",
+                "sInfoFiltered": "(จากเร็คคอร์ดทั้งหมด _MAX_ เร็คคอร์ด)",
+                "sSearch": "ค้นหา: ",
+                "oPaginate": {
+                    "sFirst":    "หน้าแรก",
+                    "sPrevious": "ก่อนหน้า",
+                    "sNext":     "ถัดไป",
+                    "sLast":     "หน้าสุดท้าย"
+                }
+            },
 					"ajax":{
 						url :"query-commandfront.php", // json datasource
 						type: "post",  // method  , by default get
