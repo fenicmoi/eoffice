941298220ea60a723eb69d6a397e888e7aa52e28
fenicmoi <phatthalung.it@gmail.com>
2026-01-19 14:55:29 +0700

    edit report

--- admin/report/rep-resive-province-mount.php
+++ admin/report/rep-resive-province-mount.php
@@ -1,8 +1,8 @@
 <?php
 
 session_start();
-require_once(__DIR__ . '/vendor/autoload.php'); 
-ob_start(); 
+require_once(__DIR__ . '/vendor/autoload.php');
+ob_start();
 
 include "../../library/config.php";
 include "../../library/database.php";
@@ -10,13 +10,13 @@ include "../function.php";
 
 
 // 2. ดึงค่าจาก POST และ Session
-$dep_id    = $_SESSION['ses_dep_id'];  
-$sec_id    = $_SESSION['ses_sec_id'];
+$dep_id = $_SESSION['ses_dep_id'];
+$sec_id = $_SESSION['ses_sec_id'];
 $dateStart = $_POST['dateStart'];   // วันที่เริ่มต้น
-$dateEnd   = $_POST['dateEnd'];     // วันที่สิ้นสุด
-$uid       = $_POST['uid'];
-$yid       = $_POST['yid'];
-$username  = $_POST['username'];
+$dateEnd = $_POST['dateEnd'];     // วันที่สิ้นสุด
+$uid = $_POST['uid'];
+$yid = $_POST['yid'];
+$username = $_POST['username'];
 
 
 $sql_header = "SELECT d.dep_name,s.sec_id,s.sec_name 
@@ -30,34 +30,49 @@ $row_header = dbFetchArray($result_header);
 
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>พิมพ์รายงานหนังสือรับประจำวัน[สารบรรณกลาง]</title>
-<link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>พิมพ์รายงานหนังสือรับประจำวัน[สารบรรณกลาง]</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <table cellspacing="0" cellpadding="1" border="0" style="width:100%;">
-        <tr> 
+        <tr>
             <td colspan="8" class="header-bg">
-                <center><h3>รายงานทะเบียนหนังสือรับ ระหว่างวันที่ <?php echo thaiDate($dateStart); ?> - <?php echo thaiDate($dateEnd);?> </h3></center>
+                <center>
+                    <h3>รายงานทะเบียนหนังสือรับ ระหว่างวันที่ <?php echo thaiDate($dateStart); ?> -
+                        <?php echo thaiDate($dateEnd); ?>
+                    </h3>
+                </center>
             </td>
-        </tr> 
-        <tr>
-            <td class="header-bg" colspan="8"><center><h4>หน่วยรับ: <?php echo $row_header['dep_name'];?></h4></center></td>
         </tr>
         <tr>
-            <td class="header-bg" colspan="8"><center><h4>กลุ่มงาน/หน่วยงานย่อย: <?php echo $row_header['sec_name'];?> &nbsp;|&nbsp; วันที่ออกรายงาน: <?php echo DateThai();?></center></td>
+            <td class="header-bg" colspan="8">
+                <center>
+                    <h4>หน่วยรับ: <?php echo $row_header['dep_name']; ?></h4>
+                </center>
+            </td>
         </tr>
         <tr>
-            <th width="5%" >#</th>
+            <td class="header-bg" colspan="8">
+                <center>
+                    <h4>กลุ่มงาน/หน่วยงานย่อย: <?php echo $row_header['sec_name']; ?> &nbsp;|&nbsp; วันที่ออกรายงาน:
+                        <?php echo DateThai(); ?>
+                </center>
+            </td>
+        </tr>
+        <tr>
+            <th width="5%">#</th>
             <th width="5%">เลขรับ</th>
             <th width="10%">วันที่รับ</th>
             <th width="10%">เลขหนังสือ</th>
             <th width="10%">ลงวันที่</th>
             <th width="40">เรื่อง</th>
             <th width="15%">หน่วยปฏิบัติ</th>
-            <th width="10%">ลงชื่อผู้รับ</th> 
+            <th width="10%">ลงชื่อผู้รับ</th>
         </tr>
         <?php
         $sql_data = "SELECT  m.book_id,m.rec_id,m.dep_id,d.book_no,d.title,d.sendfrom,d.sendto,d.date_book,d.date_in,d.date_line,d.practice,d.status,s.sec_code,dep.dep_name
@@ -65,35 +80,38 @@ $row_header = dbFetchArray($result_header);
                      INNER JOIN book_detail d ON d.book_id = m.book_id
                      INNER JOIN section s ON s.sec_id = m.sec_id 
                      INNER JOIN depart dep ON d.practice = dep.dep_id
+                     LEFT JOIN user u ON m.u_id = u.u_id
                      WHERE m.type_id=1 
-                     AND d.date_line BETWEEN DATE('$dateStart') AND DATE('$dateEnd') 
-                     AND m.dep_id='$dep_id'
+                     AND d.date_line BETWEEN '$dateStart 00:00:00' AND '$dateEnd 23:59:59' 
+                     AND (m.dep_id='$dep_id' OR u.dep_id='$dep_id')
                      ORDER BY m.rec_id DESC";
-        
+
         $result_data = dbQuery($sql_data);
         $i = 1;
-        
-        while($rs = dbFetchArray($result_data)){
-        ?>  
-        <tr>
-            <td align="center"><?=$i?></td> 
-            <td >&nbsp;<?=$rs['rec_id']?></td>
-            <td >&nbsp;<?=thaiDate($rs['date_in'])?></td>
-            <td >&nbsp;<?=$rs['book_no']?></td>
-            <td >&nbsp;<?=thaiDate($rs['date_book'])?></td>
-            <td >&nbsp;<?=$rs['title']?></td>
-            <td >&nbsp;<?=$rs['dep_name']?></td>
-            <td >&nbsp;</td>
-        </tr>
-        <?php $i++; } ?>
+
+        while ($rs = dbFetchArray($result_data)) {
+            ?>
+            <tr>
+                <td align="center"><?= $i ?></td>
+                <td>&nbsp;<?= $rs['rec_id'] ?></td>
+                <td>&nbsp;<?= thaiDate($rs['date_in']) ?></td>
+                <td>&nbsp;<?= $rs['book_no'] ?></td>
+                <td>&nbsp;<?= thaiDate($rs['date_book']) ?></td>
+                <td>&nbsp;<?= $rs['title'] ?></td>
+                <td>&nbsp;<?= $rs['dep_name'] ?></td>
+                <td>&nbsp;</td>
+            </tr>
+            <?php $i++;
+        } ?>
         <tr>
             <td class="header-bg" colspan="6" align="right"><b>รวมหนังสือรับ</b></td>
-            <td class="header-bg" colspan="2" align="center"><b><?= $i-1 ?> ฉบับ</b></td>
+            <td class="header-bg" colspan="2" align="center"><b><?= $i - 1 ?> ฉบับ</b></td>
         </tr>
     </table>
-<h4>*หมายเหตุ: ใช้สำหรับเจ้าหน้าที่นำส่งเอกสารลงชื่อรับเอกสารตัวจริง</h4>
+    <h4>*หมายเหตุ: ใช้สำหรับเจ้าหน้าที่นำส่งเอกสารลงชื่อรับเอกสารตัวจริง</h4>
 </body>
-</html> 
+
+</html>
 
 
 <?Php
@@ -103,7 +121,7 @@ $html = ob_get_clean(); // ใช้ ob_get_clean() แทน ob_end_clean() เ
 
 // **การตั้งค่า mPDF สำหรับเวอร์ชันใหม่ (ใช้ Namespace)**
 $mpdf = new \Mpdf\Mpdf([
-    'mode' => 'utf-8', 
+    'mode' => 'utf-8',
     'format' => 'A4-L', // A4-L คือแนวนอน
     'tempDir' => __DIR__ . '/temp', // แนะนำให้กำหนด Temp Directory ที่เขียนได้
     'autoScriptToLang' => true,
@@ -113,7 +131,7 @@ $mpdf = new \Mpdf\Mpdf([
     'margin_right' => 10, // ขอบขวา 10 มม.
     'margin_top' => 10,   // ขอบบน 15 มม. (เผื่อพื้นที่ส่วนหัว)
     'margin_bottom' => 10, // ขอบล่าง 15 มม. (เผื่อพื้นที่ส่วนท้าย)
-]); 
+]);
 // **หมายเหตุ:** mPDF เวอร์ชันใหม่จะไม่รับพารามิเตอร์แบบเดิมแล้ว
 
 // **ตั้งค่าฟอนต์สำหรับภาษาไทย** (สำคัญมาก)
@@ -124,7 +142,7 @@ $mpdf->SetDisplayMode('fullpage');
 $mpdf->WriteHTML($html, \Mpdf\HTMLParserMode::DEFAULT_MODE); // ใช้ค่าคงที่สำหรับ WriteHTML
 
 // สั่งให้ดาวน์โหลดไฟล์โดยใช้ชื่อไฟล์
-$mpdf->Output('รายงานหนังสือรับ_'.date('Ymd').'.pdf', \Mpdf\Output\Destination::INLINE); 
+$mpdf->Output('รายงานหนังสือรับ_' . date('Ymd') . '.pdf', \Mpdf\Output\Destination::INLINE);
 // \Mpdf\Output\Destination::INLINE จะแสดงในเบราว์เซอร์, FILE จะบันทึกเป็นไฟล์
 
 exit; // จบการทำงานหลังจากสร้าง PDF
--- admin/report/rep-resive-province.php
+++ admin/report/rep-resive-province.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require_once(__DIR__ . '/vendor/autoload.php'); 
+require_once(__DIR__ . '/vendor/autoload.php');
 ob_start();
 
 include "../../library/config.php";
@@ -10,90 +10,111 @@ include "../function.php"; // ต้องแน่ใจว่าฟังก�
 
 
 // 3. ดึงค่าจาก Session และ POST
-$dep_id   = $_SESSION['ses_dep_id'];
-$sec_id   = $_SESSION['ses_sec_id'];
-$dateprint = DATE($_POST['dateprint']);  // วันที่พิมพ์รายงาน
-$uid       = $_POST['uid'];
-$yid       = $_POST['yid'];
-$username  = $_POST['username'];
+$dep_id = $_SESSION['ses_dep_id'];
+$sec_id = $_SESSION['ses_sec_id'];
+$dateprint = $_POST['dateprint'];  // วันที่พิมพ์รายงาน
+$uid = $_POST['uid'];
+$yid = $_POST['yid'];
+$username = $_POST['username'];
 
 
 
-$sql="SELECT d.dep_name,s.sec_id,s.sec_name 
+$sql = "SELECT d.dep_name,s.sec_id,s.sec_name 
       FROM depart as d
       INNER JOIN section as s ON s.sec_id='$sec_id'
       WHERE d.dep_id = '$dep_id'";
 
-$result=dbQuery($sql);
-$row=dbFetchArray($result);
+$result = dbQuery($sql);
+$row = dbFetchArray($result);
 
 ?>
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>พิมพ์รายงานหนังสือรับประจำวัน[สารบรรณกลาง]</title>
-<link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>พิมพ์รายงานหนังสือรับประจำวัน[สารบรรณกลาง]</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
     <table cellspacing="0" cellpadding="1" border="0" style="width:100%;">
-        <tr> 
-            <td class="header-bg" colspan="8"><center><h4>รายงานทะเบียนรับหนังสือจังหวัดพัทลุง ประจำวันที่ <?= thaiDate($dateprint)?></h4></center></td>
-        </tr> 
         <tr>
-            <td class="header-bg" colspan="8"><center><h4>หน่วยรับ: <?php echo $row['dep_name'];?></h4></center></td>
+            <td class="header-bg" colspan="8">
+                <center>
+                    <h4>รายงานทะเบียนรับหนังสือจังหวัดพัทลุง ประจำวันที่ <?= thaiDate($dateprint) ?></h4>
+                </center>
+            </td>
         </tr>
         <tr>
-            <td class="header-bg" colspan="8"><center><h4>กลุ่มงาน: <?php echo $row['sec_name'];?> &nbsp;|&nbsp; วันที่ออกรายงาน: <?php echo DateThai();?></h4></center></td>
+            <td class="header-bg" colspan="8">
+                <center>
+                    <h4>หน่วยรับ: <?php echo $row['dep_name']; ?></h4>
+                </center>
+            </td>
         </tr>
         <tr>
-            <th width="5%" >#</th>
+            <td class="header-bg" colspan="8">
+                <center>
+                    <h4>กลุ่มงาน: <?php echo $row['sec_name']; ?> &nbsp;|&nbsp; วันที่ออกรายงาน:
+                        <?php echo DateThai(); ?>
+                    </h4>
+                </center>
+            </td>
+        </tr>
+        <tr>
+            <th width="5%">#</th>
             <th width="5%">เลขรับ</th>
             <th width="10%">วันที่รับ</th>
             <th width="10%">เลขหนังสือ</th>
             <th width="10%">ลงวันที่</th>
             <th width="40">เรื่อง</th>
             <th width="15%">หน่วยปฏิบัติ</th>
-            <th width="10%">ลงชื่อผู้รับ</th> 
+            <th width="10%">ลงชื่อผู้รับ</th>
         </tr>
         <?php
-        $i=1;  // ตัวนับลำดับแถว
-        $sql="SELECT m.book_id,m.rec_id,m.dep_id,d.book_no,d.title,d.sendfrom,d.sendto,d.date_book,d.date_in,d.date_line,d.practice,d.status,s.sec_code,dep.dep_name
+        $i = 1;  // ตัวนับลำดับแถว
+        $sql = "SELECT m.book_id,m.rec_id,m.dep_id,d.book_no,d.title,d.sendfrom,d.sendto,d.date_book,d.date_in,d.date_line,d.practice,d.status,s.sec_code,dep.dep_name
               FROM book_master m
               INNER JOIN book_detail d ON d.book_id = m.book_id
               INNER JOIN section s ON s.sec_id = m.sec_id 
               INNER JOIN depart dep ON dep.dep_id= d.practice
-              WHERE m.type_id=1 AND DATE(d.date_in) ='$dateprint' AND m.dep_id = '$dep_id'
+              LEFT JOIN user u ON m.u_id = u.u_id
+              WHERE m.type_id=1 
+              AND d.date_in BETWEEN '$dateprint 00:00:00' AND '$dateprint 23:59:59' 
+              AND (m.dep_id='$dep_id' OR u.dep_id='$dep_id')
               ORDER BY m.rec_id DESC";
-        
+
         // **การนับแถวด้วย dbNumRows(sql) ก่อนรัน dbQuery ซ้ำ อาจทำให้โค้ดทำงานซ้ำซ้อน**
         // ควรใช้ $i=1; และนับไปเรื่อยๆ ในลูป หรือใช้ dbNumRows(result) หลังการ Query
         
-        $result=dbQuery($sql);
-        $i=1; // เริ่มนับที่ 1 ใหม่
+        $result = dbQuery($sql);
+        $i = 1; // เริ่มนับที่ 1 ใหม่
         
-        while($rs=dbFetchArray($result)){
-        ?> 
-        <tr>
-            <td align="center"><?=$i?></td> 
-            <td >&nbsp;<?=$rs['rec_id']?></td>
-            <td >&nbsp;<?=thaiDate($rs['date_in'])?></td>
-            <td >&nbsp;<?=$rs['book_no']?></td>
-            <td >&nbsp;<?=thaiDate($rs['date_book'])?></td>
-            <td >&nbsp;<?=$rs['title']?></td>
-            <td >&nbsp;<?=$rs['dep_name']?></td>
-            <td >&nbsp;</td>
-        </tr>
-        <?php $i++; } ?> 
+        while ($rs = dbFetchArray($result)) {
+            ?>
+            <tr>
+                <td align="center"><?= $i ?></td>
+                <td>&nbsp;<?= $rs['rec_id'] ?></td>
+                <td>&nbsp;<?= thaiDate($rs['date_in']) ?></td>
+                <td>&nbsp;<?= $rs['book_no'] ?></td>
+                <td>&nbsp;<?= thaiDate($rs['date_book']) ?></td>
+                <td>&nbsp;<?= $rs['title'] ?></td>
+                <td>&nbsp;<?= $rs['dep_name'] ?></td>
+                <td>&nbsp;</td>
+            </tr>
+            <?php $i++;
+        } ?>
         <tr>
             <td class="header-bg" colspan="7" align="right"><b>รวมหนังสือรับ</b></td>
-            <td class="header-bg" colspan="1" align="center"><b><?=$i-1?> ฉบับ</b></td>
+            <td class="header-bg" colspan="1" align="center"><b><?= $i - 1 ?> ฉบับ</b></td>
         </tr>
     </table>
-<h4>*หมายเหตุ: ใช้สำหรับเจ้าหน้าที่นำส่งเอกสารลงชื่อรับเอกสารตัวจริง</h4>
+    <h4>*หมายเหตุ: ใช้สำหรับเจ้าหน้าที่นำส่งเอกสารลงชื่อรับเอกสารตัวจริง</h4>
 </body>
+
 </html>
 
 <?Php
@@ -103,7 +124,7 @@ $html = ob_get_clean(); // ใช้ ob_get_clean() แทน ob_end_clean() เ
 
 // **การตั้งค่า mPDF สำหรับเวอร์ชันใหม่ (ใช้ Namespace)**
 $mpdf = new \Mpdf\Mpdf([
-    'mode' => 'utf-8', 
+    'mode' => 'utf-8',
     'format' => 'A4-L', // A4-L คือแนวนอน
     'tempDir' => __DIR__ . '/temp', // แนะนำให้กำหนด Temp Directory ที่เขียนได้
     'autoScriptToLang' => true,
@@ -113,7 +134,7 @@ $mpdf = new \Mpdf\Mpdf([
     'margin_right' => 10, // ขอบขวา 10 มม.
     'margin_top' => 10,   // ขอบบน 15 มม. (เผื่อพื้นที่ส่วนหัว)
     'margin_bottom' => 10, // ขอบล่าง 15 มม. (เผื่อพื้นที่ส่วนท้าย)
-]); 
+]);
 // **หมายเหตุ:** mPDF เวอร์ชันใหม่จะไม่รับพารามิเตอร์แบบเดิมแล้ว
 
 // **ตั้งค่าฟอนต์สำหรับภาษาไทย** (สำคัญมาก)
@@ -124,7 +145,7 @@ $mpdf->SetDisplayMode('fullpage');
 $mpdf->WriteHTML($html, \Mpdf\HTMLParserMode::DEFAULT_MODE); // ใช้ค่าคงที่สำหรับ WriteHTML
 
 // สั่งให้ดาวน์โหลดไฟล์โดยใช้ชื่อไฟล์
-$mpdf->Output('รายงานหนังสือรับ_'.date('Ymd').'.pdf', \Mpdf\Output\Destination::INLINE); 
+$mpdf->Output('รายงานหนังสือรับ_' . date('Ymd') . '.pdf', \Mpdf\Output\Destination::INLINE);
 // \Mpdf\Output\Destination::INLINE จะแสดงในเบราว์เซอร์, FILE จะบันทึกเป็นไฟล์
 
 exit; // จบการทำงานหลังจากสร้าง PDF
